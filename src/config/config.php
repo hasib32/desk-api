67bf5a640c584e0510d18eca836df450fddef247
hasan hasibul <hasib32@gmail.com>
2015-04-29 15:16:13 -0400

    updated .gitignore to ignore .idea folder and modified config.php

--- src/config/config.php
+++ src/config/config.php
@@ -2,3 +2,20 @@
 /**
  * configuration file for hasan/desk-api
  */
+return array(
+    /**
+     * authentication system
+     * Basic or Auth
+     * basic or oauth1.0
+     */
+    'authentication'    => 'Basic',
+    /**
+     * credentials for using api
+     */
+    'credentials'       => array(
+        'url'           => '',
+        'username'      => '',
+        'password'      => ''
+    )
+);
+
